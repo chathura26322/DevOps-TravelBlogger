6d5ecd006d42a294f9b1c1ec01dab0a537a42779
Chathura Kumara <chathurakumara@192.168.1.4>
2025-01-12 18:52:31 +0530

    add the intial project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NavBar from "./Components/Navbar";
+import Login from "./Pages/Login/Login";
+
+function App() {
+  return (
+    <Login/>
+  )
+}
+
+export default App;
